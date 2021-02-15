1bc0bf3c243268d926e9ec5f7acd2de10619f7f8
felipeemidio <lipe.emidio@gmail.com>
2021-02-15 01:35:27 -0300

    inital commit

---
+++ android/app/src/main/kotlin/br/com/emidio/youtube_favorites/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.emidio.youtube_favorites
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/api.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:youtube_favorites/models/video.dart';
+
+// put your YouTube v3 API key here.
+const API_KEY = '';
+
+// "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+// "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+// "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
+
+class Api {
+
+  List<Video> decode(http.Response response) {
+    if(response.statusCode == 200) {
+      var decoded = json.decode(response.body);
+      List<Video> videos = decoded["items"].map<Video>((v) {
+        return Video.fromJson(v);
+      }).toList();
+      return videos;
+    } else {
+      throw Exception("Failed to load video!");
+    }
+  }
+
+  search(String search) async  {
+    http.Response response = await http.get(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+    );
+    decode(response);
+  }
+
+}
